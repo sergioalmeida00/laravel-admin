14cd4dd91bb1bb4c771ce3a9d610c4a8a7534975
sergioalmeida00 <sergioalmeidaa00@gmail.com>
2023-08-25 21:09:11 -0300

    REFACTOR: script create modal

--- resources/views/admin/balance/modal/create.blade.php
+++ resources/views/admin/balance/modal/create.blade.php
@@ -48,7 +48,9 @@
 
 <script>
     const formSalvar = document.querySelector('#salvar');
-    formSalvar.addEventListener('submit', async (event) => {
+    formSalvar.addEventListener('submit', handleSubmit);
+
+    async function handleSubmit(event) {
         event.preventDefault();
         const form = event.target;
         const formData = new FormData(form);
@@ -64,24 +66,29 @@
 
             if (response.ok) {
                 const responseData = await response.json();
-                if (responseData.success) {
-                    $('#exampleModal').modal('hide');
-                    window.location.href = '{{ route('admin.balance') }}';
-                } else {
-                    for (const fieldName of responseData.fields) {
-                        const field = form.querySelector(`[name="${fieldName}"]`);
-                        if (field) {
-                            field.classList.add('is-invalid');
-                        }
-                    }
-                }
-            } else {
 
+                if (responseData.success) {
+                    hideModalAndNavigate('{{ route('admin.balance') }}')
+                } else {
+                    highlightInvalidFields(form, responseData.fields)
+                }
             }
         } catch (error) {
             console.error('Erro durante a requisição:', error);
         }
-    });
+    }
 
+    function hideModalAndNavigate(route) {
+        $('#exampleModal').modal('hide');
+        window.location.href = route;
+    }
 
+    function highlightInvalidFields(form, fieldNames) {
+        for (const fieldName of fieldNames) {
+            const field = form.querySelector(`[name="${fieldName}"]`);
+            if (field) {
+                field.classList.add('is-invalid');
+            }
+        }
+    }
 </script>
