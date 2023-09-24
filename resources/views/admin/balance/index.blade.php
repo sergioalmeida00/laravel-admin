591fd7b8c97d7c830d932fdd992434873c6fc8d2
sergioalmeida00 <sergioalmeidaa00@gmail.com>
2023-09-24 07:48:56 -0300

    REFACTOR: ajustado o css progress-bar

--- resources/views/admin/balance/index.blade.php
+++ resources/views/admin/balance/index.blade.php
@@ -19,9 +19,6 @@
                 <div class="info-box-content">
                     <span class="info-box-text">Entradas</span>
                     <span class="info-box-number">R$ {{ number_format($incomeTotal, 2, '.', '') }}</span>
-                    <div class="progress">
-                        <div class="progress-bar bg-info" style="width: 70%"></div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -31,9 +28,6 @@
                 <div class="info-box-content">
                     <span class="info-box-text">Saidas</span>
                     <span class="info-box-number">R$ {{ number_format($expenseTotal, 2, '.', '') }}</span>
-                    <div class="progress">
-                        <div class="progress-bar bg-info" style="width: 70%"></div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -43,9 +37,6 @@
                 <div class="info-box-content">
                     <span class="info-box-text">Atual</span>
                     <span class="info-box-number">R$ {{ number_format($balance, 2, '.', '') }}</span>
-                    <div class="progress">
-                        <div class="progress-bar bg-info" style="width: 70%"></div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -60,13 +51,13 @@
                         <form method="POST" action="{{ route('admin.filter') }}">
                             @csrf
                             <div class="row">
-                                <div class="col-12 col-md-5">
+                                <div class="col-5 col-md-5">
                                     <input type="date" name="date_inicio" class="form-control">
                                 </div>
-                                <div class="col-12 col-md-5">
+                                <div class="col-5 col-md-5">
                                     <input type="date" name="date_fim" class="form-control">
                                 </div>
-                                <div class="col-12 col-md-2">
+                                <div class="col-2 col-md-2">
                                     <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
                                 </div>
                             </div>
@@ -86,8 +77,11 @@
                                     </p>
                                     <strong>Valor: R$ {{ number_format($transaction->amount, 2, '.', '') }}</strong>
                                     <p>
-                                        <small class="badge badge-{{ $transaction->type === 'EXPENSE' ? 'danger' : 'success' }}">
-                                            {!! $transaction->type === 'EXPENSE' ? '<i class="fas fa-minus"></i> Saida' : '<i class="fas fa-plus"></i> Entrada' !!}
+                                        <small
+                                            class="badge badge-{{ $transaction->type === 'EXPENSE' ? 'danger' : 'success' }}">
+                                            {!! $transaction->type === 'EXPENSE'
+                                                ? '<i class="fas fa-minus"></i> Saida'
+                                                : '<i class="fas fa-plus"></i> Entrada' !!}
                                         </small>
                                     </p>
 
@@ -107,17 +101,17 @@
 
                 <div class="card-body">
                     @foreach ($amountByCategory as $categoryData)
-                        <strong>
-                            {{ $categoryData['categoryName'] }}
-                            <small> - R$: {{ number_format($categoryData['total'], 2) }} </small>
-                        </strong>
+                        <div class="skill-box">
+                            <span class="title"> {{ $categoryData['categoryName'] }} - R$: {{ number_format($categoryData['total'], 2) }}</span>
 
-                        <div class="progress mb-2">
-                            <div class="progress-bar bg-primary" style="width: {{ $categoryData['percentage'] + 5 }}%">
-                                {{ number_format($categoryData['percentage'], 2) }}%
+                            <div class="skill-bar">
+                                <span class="skill-per" style="width:{{ $categoryData['percentage'] + 5}}%">
+                                    <span class="tooltip-per">{{ number_format($categoryData['percentage'], 2) }}%</span>
+                                </span>
                             </div>
                         </div>
                     @endforeach
+
                 </div>
 
             </div>
@@ -129,6 +123,9 @@
 @stop
 @section('css')
     <style>
+        :root{
+            --primary-shape:#734bd1;
+        }
         .callout {
             transition: transform 0.2s;
         }
@@ -136,6 +133,72 @@
         .callout:hover {
             transform: scale(1.02);
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        }
+
+        /* STYLE PROGRESS BAR */
+        .skill-box {
+            width: 100%;
+            margin-bottom: 1.7rem;
+        }
+
+        .skill-box .title {
+            display: block;
+            font-size: 14px;
+            font-weight: bold;
+            color: #333;
+        }
+
+        .skill-box .skill-bar {
+            height: 8px;
+            border-radius: 6px;
+            width: 100%;
+            background: rgba(0, 0, 0, 0.1);
+        }
+
+        .skill-bar .skill-per {
+            position: relative;
+            display: block;
+            height: 100%;
+            border-radius: 6px;
+            background: var(--primary-shape);
+            animation: progress 0.4s ease-out forwards;
+            opacity: 0;
+        }
+
+
+        @keyframes progress {
+            0% {
+                width: 0;
+                opacity: 0;
+            }
+
+            100% {
+                opacity: 1;
+            }
+        }
+
+        .skill-per .tooltip-per {
+            position: absolute;
+            right: -22px;
+            top: 14px;
+            font-size: 12px;
+            font-weight: bold;
+            padding: 2px 6px;
+            border-radius: 3px;
+            color: white;
+            background: var(--primary-shape);
+            z-index: 1;
+        }
+
+        .tooltip-per::before {
+            content: '';
+            position: absolute;
+            top: -2px;
+            height: 10px;
+            width: 10px;
+            z-index: -1;
+            background: var(--primary-shape);
+            transform: translateX(20%) rotate(45deg);
         }
     </style>
 @stop
