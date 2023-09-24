d83461373dc830d8eff36e5095e93de9826f1dba
sergioalmeida00 <sergioalmeidaa00@gmail.com>
2023-09-24 12:41:47 -0300

    REFACTOR: separado o script para criação de trasações

--- resources/views/admin/balance/modal/create.blade.php
+++ resources/views/admin/balance/modal/create.blade.php
@@ -65,52 +65,7 @@
     </form>
 @endsection
 
-@section('modal-script')
-    <script>
-        const formSalvar = document.querySelector('#salvar');
-        formSalvar.addEventListener('submit', handleSubmit);
-        async function handleSubmit(event) {
-            event.preventDefault();
-            const form = event.target;
-            const formData = new FormData(form);
-
-            try {
-                const response = await fetch(form.action, {
-                    method: 'POST',
-                    body: formData,
-                    headers: {
-                        'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                    }
-                });
-
-                if (response.ok) {
-                    const responseData = await response.json();
-                    console.log(responseData)
-
-                    if (responseData.success) {
-                        hideModalAndNavigate('{{ route('admin.balance') }}')
-                    } else {
-                        highlightInvalidFields(form, responseData.fields)
-                    }
-                }
-            } catch (error) {
-                console.error('Erro durante a requisição:', error);
-            }
-        }
-
-        function hideModalAndNavigate(route) {
-            $('#exampleModal').modal('hide');
-            window.location.href = route;
-        }
-
-        function highlightInvalidFields(form, fieldNames) {
-            for (const fieldName of fieldNames) {
-                const field = form.querySelector(`[name="${fieldName}"]`);
-                if (field) {
-                    field.classList.add('is-invalid');
-                }
-            }
-        }
-    </script>
-@endsection
+@section('js')
+    @include('admin.balance.scripts')
+@stop
 
